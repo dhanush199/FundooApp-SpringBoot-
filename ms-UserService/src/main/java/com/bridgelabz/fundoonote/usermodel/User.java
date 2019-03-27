b8c26a1c75b5b707541938b806e1931cd5c114c4
dhanush199 <dhanushsh1995@gmail.com>
2019-03-27 18:33:04 +0530

    AUTO rEFRESH DONE

--- ms-UserService/src/main/java/com/bridgelabz/fundoonote/usermodel/User.java
+++ ms-UserService/src/main/java/com/bridgelabz/fundoonote/usermodel/User.java
@@ -2,18 +2,12 @@ package com.bridgelabz.fundoonote.usermodel;
 
 
 import java.io.Serializable;
-import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
 import javax.persistence.Lob;
-import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
